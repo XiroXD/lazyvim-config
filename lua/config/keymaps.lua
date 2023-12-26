91718d2f01bbb8c3fdab149dfa73f2e8fe38510e
Xiro <xiro@duck.com>
2023-12-26 16:14:24 +0100

    added keymaps for crates plugin

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,9 @@
--- Keymaps are automatically loaded on the VeryLazy event
--- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
+local crates = require("crates")
+
+local map = vim.keymap.set
+
+map("n", "<leader>cRr", crates.reload, { silent = true, desc = "Reload crates.io index" })
+map("n", "<leader>cRv", crates.show_versions_popup, { silent = true, desc = "Show crate versions" })
+map("n", "<leader>cRf", crates.show_features_popup, { silent = true, desc = "Show crate features" })
+map("n", "<leader>cRd", crates.show_dependencies_popup, { silent = true, desc = "Show crate dependencies" })
+map("n", "<leader>cRD", crates.open_documentation, { silent = true, desc = "Open crate documentation" })
